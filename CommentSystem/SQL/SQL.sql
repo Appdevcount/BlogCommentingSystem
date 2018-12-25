a4d02be21b7de8e1794703ca0aa67aca6189bebe
APPDEVCOUNT <sira.deen@gmail.com>
2018-12-25 12:25:26 +0300

    Add project files.

---
+++ CommentSystem/SQL/SQL.sql
@@ -0,0 +1,25 @@
+CommentId which is the primary key, CommentText which holds the comment itself, 
+Username which store the author of the comment, CommentDate and ParentId.
+
+Create Table Comment(
+CommentId bigint identity primary key,
+CommentText nvarchar(max),
+Username nvarchar(max),
+CommentDate datetime,
+ParentId bigint
+)
+
+
+--Can add a PostID Column to relate comments to post
+--Can add a DisplayCommentFlag to display/hide inappropriate comments
+--Can enable comment form only when authenticated - can provide google auth and Cutom login icon at starting as auth options for 
+--comment form
+--Can use authentcated userid/name as Username while submitting comments
+--Can display empty image in comments section, that's not important for blog content
+
+
+select * from Comment
+delete from Comment
+
+
+delete from Comment where ParentId=49
